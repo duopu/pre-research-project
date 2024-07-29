5e6be8906b1e31db317045f0fe8a3c6340f7228e
jiangeq <jiangeq@digiwin.com>
2024-07-29 13:50:44 +0800

    feat: init

--- src/chat/chat1/script.js
+++ src/chat/chat1/script.js
@@ -1,8 +1,15 @@
+let num = 0;
+
 function insertDiv() {
+
+    num++;
+
     const list = document.getElementById('list');
     const newDiv = document.createElement('div');
     const newListItem = document.createElement('li');
     const itemCount = list.children.length;
+
+    newListItem.id = `${ num }`;
 
     // 设置背景颜色
     if (itemCount % 2 === 0) {
@@ -10,6 +17,8 @@ function insertDiv() {
     } else {
         newDiv.classList.add('animated-div', 'even');
     }
+
+    newDiv.innerHTML = num;
 
     newListItem.appendChild(newDiv);
     list.appendChild(newListItem);
@@ -23,12 +32,42 @@ function insertDiv() {
     });
 }
 
+const timeCell = 500;
+
 function removeDiv(listItem) {
     const div = listItem.querySelector('.animated-div');
-    div.classList.add('remove');
+
+    // let nextId = parseInt(listItem.id) + 1;
+    // const nextEle = document.getElementById(nextId)
+
+    // console.log('nummnummnumm', div.innerHTML, listItem, nextId, listItem.id, nextEle);
+
+
+    // nextEle.classList.add('remove111')
+
+    listItem.classList.add('remove');
+
+    const offsetHeight = listItem.offsetHeight;
+
+    console.log('nummnummnumm', offsetHeight);
+    // div.classList.add('remove');
+
+    const onceReduceHeight = offsetHeight / 100;
+    const cell = timeCell / 100;
+    let num = 0;
+
+    let i1 = setInterval(() => {
+        num++;
+        // clearTimeout(timer1);
+        let hh = (offsetHeight - (onceReduceHeight * num)) + 'px';
+        console.log('nummnummnumm', hh);
+        listItem.style.height = hh;
+    }, cell);
 
     // 在动画结束后删除元素
     setTimeout(() => {
         listItem.remove();
-    }, 1000); // 动画持续时间为1.5秒
+        clearInterval(i1);
+        // nextEle.classList.remove('remove111');
+    }, timeCell); // 动画持续时间为1.5秒
 }
